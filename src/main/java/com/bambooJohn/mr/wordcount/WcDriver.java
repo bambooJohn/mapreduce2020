cf821c9038c7ae5729c593cf6c6e68f897bfcf0c
liangzhen <1033855573@qq.com>
2020-11-29 11:17:12 +0800

    在代码中采用压缩；测试好像没见到效果，可能要xml配置同步才行

--- src/main/java/com/bambooJohn/mr/wordcount/WcDriver.java
+++ src/main/java/com/bambooJohn/mr/wordcount/WcDriver.java
@@ -4,6 +4,8 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.SnappyCodec;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -21,10 +23,24 @@ public class WcDriver {
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
 
         //1.设置Job实例
-        Job job = Job.getInstance(new Configuration());
+        Configuration configuration = new Configuration();
+
+        configuration.set("fs.defaultFS","hdfs://hadoop102:8020");
+        configuration.set("mapreduce.framework.name","yarn");
+        configuration.set("mapreduce.app-submission.cross-platform","true"); // 允不允许从windows向Linux提交任务
+        configuration.set("yarn.resourcemanager.hostname","hadoop103");
+        Job job = Job.getInstance(configuration);
+
+        configuration.setBoolean("mapreduce.map.output.compress",true);
+        configuration.setClass("mapreduce.map.output.compress.code", SnappyCodec.class, CompressionCodec.class);
+
+        configuration.setBoolean("mapreduce.output.fileoutputformat.compress",true);
+        configuration.setClass("mapreduce.output.fileoutputformat.compress.code", SnappyCodec.class, CompressionCodec.class);
+
 
         //2.设置Jar包
-        job.setJarByClass(WcDriver.class);
+     //   job.setJarByClass(WcDriver.class);
+        job.setJar("D:\\programs\\JavaProject\\IdeaProjects\\mapreduce2020\\target\\mapreduce2020-1.0-SNAPSHOT.jar");
 
         //3.设置Mapper和Reducer
         job.setMapperClass(WcMapper.class);
@@ -39,12 +55,12 @@ public class WcDriver {
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
 
-        job.setNumReduceTasks(3);
+      //  job.setNumReduceTasks(3);
         //5.设置输入输出文件
-     //   FileInputFormat.setInputPaths(job,new Path(args[0]));
-     //   FileOutputFormat.setOutputPath(job,new Path(args[1]));
-        FileInputFormat.setInputPaths(job,new Path("D:\\input"));
-        FileOutputFormat.setOutputPath(job,new Path("D:\\output"));
+        FileInputFormat.setInputPaths(job,new Path(args[0]));
+        FileOutputFormat.setOutputPath(job,new Path(args[1]));
+     //   FileInputFormat.setInputPaths(job,new Path("D:\\input"));
+     //   FileOutputFormat.setOutputPath(job,new Path("D:\\output"));
 
         //6.提交Job
         boolean b = job.waitForCompletion(true);
